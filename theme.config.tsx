b76917e7cbcc67b4f76778f46698610da7f1e2ee
Ludaro <waschmaschvanlu@gmail.com>
2023-08-12 03:18:11 +0200

    added logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Ludaro-Wiki</span>,
+  logo:    <>
+  <img width="24" height="24" viewBox="0 0 24 24" src="poro.png"></img>
+  <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+   Ludaro-Wiki
+  </span>
+</>,
   titleTemplate: '%s – Ludaro-Wiki(ENG)',
   project: {
     link: 'https://github.com/waschmaschvanlu/ludaro-wiki-nextra-english/tree/eng',
