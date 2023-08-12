24efb47e90e0484d104b52732e4b0e3dcffe9801
Ludaro <waschmaschvanlu@gmail.com>
2023-08-12 03:15:15 +0200

    added stuff to the config

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Ludaro-Wiki</span>,
+  logo: <img  width="24" height="24" src="poro.png"></img>,
+  titleTemplate: '%s – Ludaro-Wiki(ENG)',
   project: {
-    link: 'https://github.com/waschmaschvanlu/ludaro-wiki-nextra-english/tree/main',
+    link: 'https://github.com/waschmaschvanlu/ludaro-wiki-nextra-english/tree/eng',
   },
-  docsRepositoryBase: 'https://github.com/waschmaschvanlu/ludaro-wiki-nextra-english/tree/main',
+  docsRepositoryBase: 'https://github.com/waschmaschvanlu/ludaro-wiki-nextra-english/tree/eng',
   footer: {
     text: 'Nextra Ludaro-Wiki',
   },
+  faviconGlyph: '📚',
 }
 
 export default config
